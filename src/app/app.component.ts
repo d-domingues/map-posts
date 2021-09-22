95031f7c46b71ca6090d3b42f332c8b26400694b
David Domigues <ddomingues@ampere-energy.com>
2021-09-22 14:45:08 +0200

    initial map setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { MapInfoWindow, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+    <google-map [options]="options" width="100vw" height="100vh" (mapClick)="onClick($event)">
+      <!-- MARKERS -->
+      <map-marker
+        #marker="mapMarker"
+        *ngFor="let markerPosition of markerPositions"
+        [position]="markerPosition"
+        [options]="markerOptions"
+        (mapClick)="onOpenInfo(marker, info)"
+      ></map-marker>
+      <!-- INFO -->
+      <map-info-window #info="mapInfoWindow">
+        <input type="text" />
+      </map-info-window>
+    </google-map>
+  `,
 })
 export class AppComponent {
-  title = 'map-posts';
+  options: google.maps.MapOptions = {
+    center: { lat: 40.4378698, lng: -3.8196208 },
+    zoom: 12,
+  };
+
+  markerOptions: google.maps.MarkerOptions = { draggable: false };
+  markerPositions: google.maps.LatLngLiteral[] = [];
+
+  onClick(event: google.maps.MapMouseEvent) {
+    this.markerPositions.push(event.latLng.toJSON());
+  }
+
+  onOpenInfo(marker: MapMarker, infoWindow: MapInfoWindow) {
+    infoWindow.open(marker);
+  }
 }
